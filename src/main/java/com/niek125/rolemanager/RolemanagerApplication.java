6eec453c41efd5c6a4d0afc63459f9d210714d7e
Niek125 <niek.sleddens@gmail.com>
2019-12-03 10:07:05 +0100

    initial commit

---
+++ src/main/java/com/niek125/rolemanager/RolemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.niek125.rolemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RolemanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niek125/rolemanager/RolemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niek125.rolemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
